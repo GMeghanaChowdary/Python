2eea9ef01786979d4f13b8509dd5b8dbc2235736
GMeghanaChowdary <167081845+GMeghanaChowdary@users.noreply.github.com>
2024-12-14 21:04:18 +0530

    Create movie recommendation system.py

---
+++ movie recommendation system.py
@@ -0,0 +1,5 @@
+import random
+movies = ["Inception", "Titanic", "Avatar", "The Dark Knight", "The Matrix"]
+def movie_recommender():
+    print(f"Recommended movie: {random.choice(movies)}")
+movie_recommender()
